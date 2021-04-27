2ce3674e4ecb2766167fd5632b918885651b02b7
k-emiko <62057364+k-emiko@users.noreply.github.com>
2021-04-27 17:40:30 +1100

    Address comments

--- src/test/java/manager/ProductManagerTest.java
+++ src/test/java/manager/ProductManagerTest.java
@@ -13,14 +13,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProductManagerTest {
     ProductRepository repo = new ProductRepository();
     Smartphone testPhone1 = new Smartphone(1, "EG102", 12300, "Samsung");
-    Book testBook1 = new Book(2, "Herlock Sholmes", 1200, "Capcom");
     Smartphone testPhone2 = new Smartphone(1, "EG103", 12300, "Apple");
+    Smartphone testPhone3 = new Smartphone(1, "EG103", 12300, "Samsung");
+    Book testBook1 = new Book(2, "Herlock Sholmes", 1200, "Capcom");
     Book testBook2 = new Book(2, "Compra", 1200, "Konami");
+
 
     @BeforeEach
     public void setUp() {
         repo.save(testPhone1);
         repo.save(testBook1);
+        repo.save(testPhone2);
+        repo.save(testBook2);
+        repo.save(testPhone3);
     }
 
     @AfterEach
@@ -43,18 +48,28 @@ class ProductManagerTest {
         Product[] expected = new Product[]{testBook1};
         assertArrayEquals(actual, expected);
     }
+
     @Test
     void searchByBrandTest() {
         String text = "Samsung";
         Product[] actual = ProductManager.searchBy(text);
-        Product[] expected = new Product[]{testPhone1};
+        Product[] expected = new Product[]{testPhone1, testPhone3};
         assertArrayEquals(actual, expected);
     }
+
     @Test
     void searchByPhoneNameTest() {
         String text = "EG102";
         Product[] actual = ProductManager.searchBy(text);
         Product[] expected = new Product[]{testPhone1};
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    void searchByNothingFound() {
+        String text = "123";
+        Product[] actual = ProductManager.searchBy(text);
+        Product[] expected = new Product[0];
         assertArrayEquals(actual, expected);
     }
 
